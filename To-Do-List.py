a225297fa0a51813c413bba143643c51195ccae2
89Aman <94701256+89Aman@users.noreply.github.com>
2024-05-22 21:39:37 +0530

    OOPs implementation

--- To-Do-List.py
+++ To-Do-List.py
@@ -1,29 +1,38 @@
-print("To Do List")
+class todo:
+    tasks = []
 
-tasks = []
-def create():
-    task = input("Enter your task - ")
-    task.append(tasks + "/n")
+    def create(self):
+        task = input("Enter your task - " )
+        self.tasks.append(task)
 
 
-def update():
-    print("Select The serial number from below  which you want to delete")
-    to_del = input("enter -")
-    tasks.pop(to_del)
+    def update(self):
+        print("Select The serial number from below  which you want to delete")
+        to_del = int(input("enter -"))
+        self.tasks.pop(to_del)
 
-def track():
-    print(tasks)
+    def track(self):
+        for data_fetch in self.tasks:
+            print(data_fetch)
 
-while True:
-    user_input = input("Enter the selection /n1.Create Tasks /n2.Update Tasks /n3.Track Tasks")
-    if user_input == "1":
-        create()
-    elif user_input == "2":
-        update()
-    elif user_input =="3":
-        track()
-    else:
-        print("invalid Selection")
 
+
+def main():
+    while True:
+        user_input = input("To-Do List : - \n 1.Create Tasks \n 2.Update Tasks \n 3.Track Tasks \n4.To Quit \nEnter -")
+        if user_input == "1":
+            todo.self.create()
+        elif user_input == "2":
+            todo.self.update()
+        elif user_input =="3":
+            todo.self.track()
+        elif user_input =="4":
+            print("Exitting")
+            quit()
+        else:
+            print("invalid Selection")
+
+if __name__ ==" __main__":
+    main()
     
     
