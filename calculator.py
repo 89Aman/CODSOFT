1456d463f274803d40cf771862e50a9cbffec48d
89Aman <94701256+89Aman@users.noreply.github.com>
2024-05-23 12:18:08 +0530

    Operators selection is changed

--- calculator.py
+++ calculator.py
@@ -1,20 +1,20 @@
-operations = ["1.+ \n 2.-  \n3.*  \n4./"]
+operations = ["1.ADDITION\n2.SUBSTRACTION\n3.MULTIPLICATION\n4.DIVISION"]
 for operators in operations:
     print(operators)
 
  
 num1= int(input("Enter The First Number - "))
 num2 = int(input("Enter The Second Number - "))
-oprtn = input("Enter Arithmatic Operator  serial number - ")
+oprtn = input("Enter Arithmatic Operator's serial number - ")
 if oprtn =="1":
     result = num1 +num2
-if oprtn =="2":
+    print(f"The sum of {num1} and {num2}  are {result}")
+elif oprtn =="2":
     result = num1 - num2
-if oprtn =="3":
+    print(f"The subtraction of {num1} and {num2}  are {result}")
+elif oprtn =="3":
     result = num1 * num2
-if oprtn =="4":
+    print(f"The multiflication of {num1} and {num2}  are {result}")
+elif oprtn =="4":
     result = num1  / num2
-else:
-    result = print("Invalid Entry")
-
-print(result)
+    print(f"The division of {num1} and {num2}  are {result}")
